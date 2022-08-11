8d91dbf7233c76346a0c98ae8be15431f5ad0bb4
AhmedNasserG <ahmednasser217217@gmail.com>
2022-08-11 17:31:37 +0200

    feat: Load courses data dynamically from server

--- script.js
+++ script.js
@@ -1,63 +1,49 @@
-const coursesJson = {
-	courses: [
-		{
-			id: 1,
-			title: 'python from zero to hero',
-			author: 'maximlian',
-			image: 'https://s.udemycdn.com/premium-clp/567828/CourseImage-2x.jpg',
-		},
-		{
-			id: 2,
-			title: 'javascript from zero to hero',
-			author: 'stephen',
-			image: 'https://www.pragimtech.com/wp-content/uploads/2019/03/java-script.jpg',
-		},
-		{
-			id: 3,
-			title: 'Learn Python: Python for Beginners',
-			author: 'Abrar Hussain',
-			image: 'https://img-c.udemycdn.com/course/240x135/426570_1b91_3.jpg',
-		},
-		{
-			id: 4,
-			title: 'Learn Python for Data Analysis and Visualization',
-			author: 'Jose Portila',
-			image: 'https://img-c.udemycdn.com/course/240x135/396876_cc92_7.jpg',
-		},
-		{
-			id: 5,
-			title: 'Python for Beginners - Learn Programming from scratch',
-			author: 'Edwin Diaz, Coding Faculty Solutions',
-			image: 'https://img-c.udemycdn.com/course/240x135/405878_e5a0_3.jpg',
-		},
-	],
-};
+const DATA_URL = 'http://localhost:3000/courses';
 
-const coursesListElement = document.getElementById('courses-list');
-
-coursesJson.courses.forEach((course) => {
-	const courseCardElement = createCourseCardElement(course);
-	coursesListElement.appendChild(courseCardElement);
+fetchCourses().then((coursesJsonData) => {
+	const COURSES_CARDS_LIST_ELEMENT = document.getElementById('courses-list');
+	coursesJsonData.forEach((course) => {
+		const COURSE_CARD_ELEMENT = createCourseCardElement(course);
+		COURSES_CARDS_LIST_ELEMENT.appendChild(COURSE_CARD_ELEMENT);
+	});
 });
 
-function createCourseCardElement(course) {
-	const courseImg = course.image;
-	const courseTitle = course.title;
-	const courseAuthor = course.author;
-	const courseItem = document.createElement('div');
-	courseItem.classList.add('course-card');
-	courseItem.innerHTML = `
-      <img class="course-img" src="${courseImg}" />
-      <span class="course-title">${courseTitle}</span>
-      <span class="course-subtitle">${courseAuthor}</span>
+/**
+ * @description fetch courses data from server in json format
+ * @returns {Promise} Course data in JSON format
+ */
+function fetchCourses() {
+	return new Promise((resolve, reject) => {
+		fetch(DATA_URL)
+			.then((response) => response.json())
+			.then((jsonData) => resolve(jsonData))
+			.catch((error) => reject(error));
+	});
+}
+
+/**
+ * @description create course card element
+ * @param {JSON} courseJsonData
+ * @returns {HTMLElement} Course card element
+ */
+function createCourseCardElement(courseJsonData) {
+	const COURSE_IMG = courseJsonData.image;
+	const COURSE_TITLE = courseJsonData.title;
+	const COURSE_AUTHOR = courseJsonData.author;
+	const COURSE_CARD_ELEMENT = document.createElement('div');
+	COURSE_CARD_ELEMENT.classList.add('course-card');
+	COURSE_CARD_ELEMENT.innerHTML = `
+      <img class="course-img" src="${COURSE_IMG}" />
+      <span class="course-title">${COURSE_TITLE}</span>
+      <span class="course-subtitle">${COURSE_AUTHOR}</span>
   `;
-	return courseItem;
+	return COURSE_CARD_ELEMENT;
 }
 
 // dismiss search results on click outside of the search results
 document.addEventListener('click', function (event) {
-	const navbarElement = document.getElementsByClassName('navbar')[0];
-	if (!navbarElement.contains(event.target)) {
+	const NAVBAR_ELEMENT = document.getElementsByClassName('navbar')[0];
+	if (!NAVBAR_ELEMENT.contains(event.target)) {
 		document.getElementById('search-results').style.display = 'none';
 	}
 });
@@ -65,35 +51,52 @@ document.addEventListener('click', function (event) {
 // search on enter key pressed
 document.getElementById('search-box').addEventListener('keyup', function (event) {
 	if (event.keyCode === 13) {
-		onSearchClicked();
+		onSearchClickedHandler();
 	}
 });
 
-// handle search click event
-function onSearchClicked() {
-	const searchTerm = document.getElementById('search-box').value;
-	const resultCourses = searchCourses(searchTerm);
-	const resultsListElement = document.getElementById('results-list');
-	resultsListElement.innerHTML = '';
-	resultCourses.forEach((course) => {
-		const resultElement = createResultElement(course);
-		resultsListElement.appendChild(resultElement);
+/**
+ * @description handle search click event
+ */
+function onSearchClickedHandler() {
+	const SEARCH_TERM = document.getElementById('search-box').value;
+
+	fetchCourses().then((coursesJsonData) => {
+		const FILTERED_COURSES_JSON_DATA = filterCoursesJsonData(coursesJsonData, SEARCH_TERM);
+
+		const SEARCH_RESULTS_LIST_ELEMENT = document.getElementById('results-list');
+		SEARCH_RESULTS_LIST_ELEMENT.innerHTML = ''; // clear old search results
+
+		FILTERED_COURSES_JSON_DATA.forEach((course) => {
+			const SEARCH_RESULT_ELEMENT = createSearchResultElement(course);
+			SEARCH_RESULTS_LIST_ELEMENT.appendChild(SEARCH_RESULT_ELEMENT);
+		});
+		const SEARCH_RESULTS_CONTAINER_ELEMENT = document.getElementById('search-results');
+		SEARCH_RESULTS_CONTAINER_ELEMENT.style.display = 'block';
 	});
-	document.getElementById('search-results').style.display = 'block';
 }
 
-// search for courses using search term
-function searchCourses(searchTerm) {
-	const searchResults = coursesJson.courses.filter((course) => {
+/**
+ * @description filter courses json data using search term
+ * @param {JSON} coursesJsonData
+ * @param {String} searchTerm
+ * @returns {JSON} filtered courses json data
+ */
+function filterCoursesJsonData(coursesJsonData, searchTerm) {
+	return coursesJsonData.filter((course) => {
 		return course.title.toLowerCase().includes(searchTerm.toLowerCase());
 	});
-	return searchResults;
 }
 
-function createResultElement(course) {
-	const courseTitle = course.title;
-	const courseItem = document.createElement('li');
-	courseItem.classList.add('result-item');
-	courseItem.innerHTML = `${courseTitle}`;
-	return courseItem;
+/**
+ * @description create search result element
+ * @param {JSON} courseJsonData
+ * @returns {HTMLElement} Search result element
+ */
+function createSearchResultElement(courseJsonData) {
+	const COURSE_TITLE = courseJsonData.title;
+	const SEARCH_RESULT_ELEMENT = document.createElement('li');
+	SEARCH_RESULT_ELEMENT.classList.add('result-item');
+	SEARCH_RESULT_ELEMENT.innerHTML = `${COURSE_TITLE}`;
+	return SEARCH_RESULT_ELEMENT;
 }
